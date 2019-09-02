bb8e9288b69625085517066fa00d85926b3b6609
Victor Arowo <arowov@gmail.com>
2019-09-02 13:42:12 +0100

    Completed MVP

--- js/index.js
+++ js/index.js
@@ -41,7 +41,54 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
-let nav = document.getElementsByClassName("nav-item");
+let nav = document.getElementsByTagName("nav");
+
+
 for (let i = 0; i < 6; i++){
-  nav[i].textContent = siteContent["nav"][`nav-item-${i+1}`];
+  nav[0].children[i].textContent = siteContent["nav"][`nav-item-${i+1}`];
+  nav[0].children[i].classList.add("green"); 
 }
+
+let cta = document.querySelector(".cta-text");
+
+cta.children[0].innerHTML= siteContent.cta.h1.split(" ").join("<br/>");
+cta.children[1].textContent = siteContent.cta.button;
+
+let cta_img = document.getElementById("cta-img");
+cta_img.src = siteContent.cta["img-src"];
+
+let topContent = document.querySelector(".top-content");
+topContent.children[0].children[0].textContent = siteContent["main-content"]["features-h4"];
+topContent.children[0].children[1].textContent = siteContent["main-content"]["features-content"];
+
+topContent.children[1].children[0].textContent = siteContent["main-content"]["about-h4"];
+topContent.children[1].children[1].textContent = siteContent["main-content"]["about-content"];
+
+let middleImg = document.getElementById("middle-img");
+middleImg.src = siteContent["main-content"]["middle-img-src"];
+
+let bottomContent = document.querySelector(".bottom-content");
+bottomContent.children[0].children[0].textContent = siteContent["main-content"]["services-h4"];
+bottomContent.children[0].children[1].textContent = siteContent["main-content"]["services-content"];
+
+bottomContent.children[1].children[0].textContent = siteContent["main-content"]["product-h4"];
+bottomContent.children[1].children[1].textContent = siteContent["main-content"]["product-content"];
+
+bottomContent.children[2].children[0].textContent = siteContent["main-content"]["vision-h4"];
+bottomContent.children[2].children[1].textContent = siteContent["main-content"]["vision-content"];
+
+let contact = document.querySelector(".contact");
+for(let i = 0; i < 4; i++){
+  contact.children[i].textContent = siteContent["contact"][Object.keys(siteContent["contact"])[i]];
+}
+
+let footer = document.getElementsByTagName("footer");
+footer[0].children[0].textContent = siteContent["footer"]["copyright"];
+
+let nav1 = document.createElement("a");
+let nav2 = document.createElement("a");
+nav1.textContent = "item 1";
+nav2.textContent = "item 2";
+
+nav[0].appendChild(nav1);
+nav[0].prepend(nav2);
